6b10c729ccf1d8042643efde918d3bffea6cb468
Paul <paul.vasseur23@outlook.fr>
2016-03-10 15:27:11 +0100

    class User done

---
+++ src/Aston/BackBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Aston\BackBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * Description of User
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser {
+    /**
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GenerateValue(strategy="AUTO")
+     * 
+     */
+    protected $id;
+    
+   
+}
